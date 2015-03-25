1f4cc046a65a6abe1f0a692cc06f16bfbcad18f1
weima-sage <wei.ma@sage.com>
2015-03-25 16:41:33 -0700

    Added password resets & email configuration

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,14 +5,18 @@ class UserMailer < ApplicationMailer
     mail to: "to@example.org"
   end
 
-  def password_reset
-    @greeting = "Hi"
-    mail to: "to@example.org"
+  def password_reset(user)
+    update_user_and_send_mail user, 'Password reset'
   end
 
   def account_activation(user)
-    @user = user
-    mail to: user.email, subject: 'Account activation'
+    update_user_and_send_mail user, 'Account activation'
   end
+
+  private 
+    def update_user_and_send_mail(user, subject)
+      @user = user
+      mail to: user.email, subject: subject
+    end
 
 end
